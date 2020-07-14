c7cb5ace803d60accc829480b064f61fe50ab73d
Louis Tiao <louistiao@gmail.com>
2020-07-14 22:14:09 +0200

    removed irrelevant functions

--- bore/datasets/__init__.py
+++ bore/datasets/__init__.py
@@ -1,17 +1,9 @@
-from .base import (mauna_loa_load_dataframe,
-                   coal_mining_disasters_load_data,
-                   load_bee_dance_dataframe,
-                   binary_mnist_load_data,
-                   read_fcnet_data)
+from .base import read_fcnet_data
 from .synthetic import make_regression_dataset, make_classification_dataset
 
 
 __all__ = [
     "make_regression_dataset",
     "make_classification_dataset",
-    "mauna_loa_load_dataframe",
-    "coal_mining_disasters_load_data",
-    "load_bee_dance_dataframe",
-    "binary_mnist_load_data",
     "read_fcnet_data"
 ]
