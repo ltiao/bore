fixed batch optimization (wrong sign) and added features

bore/mixins.py
@@ -10,11 +10,13 @@ class MaximizableMixin:
 
     def __init__(self, transform=tf.identity, *args, **kwargs):
         super(MaximizableMixin, self).__init__(*args, **kwargs)
-        self._func = convert(self, transform=lambda u: transform(-u))
+        # negate to turn into minimization problem for ``scipy.optimize``
+        # interface
+        self._func_min = convert(self, transform=lambda u: transform(-u))
 
     def maxima(self, bounds, num_starts=5, num_samples=1024, method="L-BFGS-B",
                options=dict(maxiter=1000, ftol=1e-9), random_state=None):
-        return minimize_multi_start(self._func, bounds=bounds,
+        return minimize_multi_start(self._func_min, bounds=bounds,
                                     num_starts=num_starts,
                                     num_samples=num_samples,
                                     random_state=random_state,
@@ -47,11 +49,16 @@ class MaximizableMixin:
 
 class BatchMaximizableMixin(MaximizableMixin):
 
+    def __init__(self, transform=tf.identity, *args, **kwargs):
+        super(BatchMaximizableMixin, self).__init__(transform=transform, *args, **kwargs)
+        # maximization problem for SVGD
+        self._func_max = convert(self, transform=lambda u: transform(u))
+
     def argmax_batch(self, batch_size, bounds, length_scale=None, n_iter=1000,
                      step_size=1e-3, alpha=.9, eps=1e-6):
 
         def log_prob_grad(x):
-            val, grad = self._func(x)
+            _, grad = self._func_max(x)
             return grad
 
         kernel = RadialBasis(length_scale=length_scale)

bore/optimizers/svgd/base.py
@@ -1,22 +1,77 @@
 import numpy as np
 
+from abc import ABC, abstractmethod
+from sklearn.utils import check_random_state
+
 from .kernels import RadialBasis
 from ..utils import from_bounds
 
-from sklearn.utils import check_random_state
+
+class Distortion(ABC):
+
+    @abstractmethod
+    def __call__(self, beta):
+        pass
+
+
+class DistortionIdentity(Distortion):
+
+    def __call__(self, beta):
+        return beta
+
+
+class DistortionExpDecay(Distortion):
+    """
+    Importance weight or distortion function :math:`omega(\beta)`
+    """
+    def __init__(self, lambd=1.):
+        assert lambd > 0, "lambda must be positive!"
+        self.lambd = lambd
+
+    def __call__(self, beta):
+        return np.pow(beta, -self.lambd)
+
+
+def rank(a):
+    """
+    Compute empirical CDF of entries in an array.
+
+    Examples
+    --------
+
+    >>> a = np.array([0.4532752, 0.858725 , 0.3792093, 0.6631048, 0.7619765])
+    >>> rank(a)
+    array([0.4, 1. , 0.2, 0.6, 0.8])
+
+    Handling duplicate values:
+
+    >>> a = np.array([0.4532752, 0.858725 , 0.3792093, 0.3792093, 0.7619765])
+    >>> rank(a)
+    array([0.6, 1. , 0.4, 0.4, 0.8])
+
+    Comparison to behaviour of ``scipy.stats.percentileofscore``:
+
+    >>> from scipy.stats import percentileofscore
+    >>> np.array_equal(100. * rank(a),
+    ...                [percentileofscore(a, x, "weak") for x in a])
+    True
+    """
+    assert a.ndim == 1, "only support 1d arrays!"
+    return np.less_equal(a, np.expand_dims(a, axis=1)).mean(axis=1)
 
 
 class SVGD:
 
     def __init__(self, kernel=RadialBasis(), n_iter=1000, step_size=1e-3,
-                 alpha=.9, eps=1e-6):
+                 tau=1., alpha=.9, eps=1e-6):
         self.kernel = kernel
         self.n_iter = n_iter
         self.step_size = step_size
+        self.tau = tau
         self.alpha = alpha
         self.eps = eps
 
-    def optimize_from_init(self, log_prob_grad, x_init, bounds=None):
+    def optimize_from_init(self, log_prob_grad, x_init, bounds=None, callback=None):
         """
         Optimize from specified starting points.
         """
@@ -30,9 +85,10 @@ class SVGD:
 
             K, K_grad = self.kernel.value_and_grad(x)
 
-            grad = (K @ log_prob_grad(x) + K_grad)
+            grad = (K @ log_prob_grad(x) + self.tau * K_grad)
             grad /= n_init
 
+            # adadelta / adagrad
             if grad_hist is None:
                 grad_hist = grad**2
             else:
@@ -41,6 +97,9 @@ class SVGD:
 
             adj_grad = np.true_divide(grad, self.eps + np.sqrt(grad_hist))
             x += self.step_size * adj_grad
+
+            if callback is not None:
+                callback(x)
 
         return x
 