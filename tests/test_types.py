c6547b37370cbfadec43f5ef90ce2fa5eb1ed2af
Louis Tiao <louistiao@gmail.com>
2021-06-16 14:04:21 +0200

    added tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -44,8 +44,8 @@ def test_shapes(config_space, seed):
 
     bounds = cs_dense.get_bounds()
 
-    np.testing.assert_array_almost_equal(bounds.lb, np.zeros(DENSE_DIM))
-    np.testing.assert_array_almost_equal(bounds.ub, np.ones(DENSE_DIM))
+    np.testing.assert_array_equal(bounds.lb, np.zeros(DENSE_DIM))
+    np.testing.assert_array_equal(bounds.ub, np.ones(DENSE_DIM))
 
     assert isinstance(cs_dense.sample_configuration(), CS.Configuration)
     assert isinstance(cs_dense.sample_configuration(size=1), CS.Configuration)
@@ -57,3 +57,50 @@ def test_shapes(config_space, seed):
     for config in configs:
         assert isinstance(config, CS.Configuration)
         assert config.to_array().shape == (DENSE_DIM,)
+
+
+def test_dense_encoding(config_space, seed):
+
+    ind = 0
+
+    cs_dense = DenseConfigurationSpace(config_space, seed=seed)
+
+    name = cs_dense.get_hyperparameter_by_idx(ind)
+    # hp = cs_dense.get_hyperparameter(name)
+    assert name == "activation_fn_1"
+
+    dct = {
+        'activation_fn_1': 'relu',
+        'activation_fn_2': 'tanh',
+        'batch_size': 2,
+        'dropout_1': 0.39803953082292726,
+        'dropout_2': 0.022039062686389176,
+        'init_lr': 0,
+        'lr_schedule': 'cosine',
+        'n_units_1': 5,
+        'n_units_2': 1
+    }
+    config = DenseConfiguration(cs_dense, values=dct)
+    array = config.to_array()
+
+    # should always be between 0 and 1
+    assert np.less_equal(0., array).all()
+    assert np.less_equal(array, 1.).all()
+
+    np.testing.assert_array_almost_equal(array, [0., 1., 1., 0., 0.62500063, 0.44226615, 0.02448785, 0.08333194, 1., 0., 0.91666806, 0.24999917])
+
+    # make sure we recover original dictionary exactly
+    config_recon = DenseConfiguration.from_array(cs_dense, array)
+    dct_recon = config_recon.get_dictionary()
+
+    assert dct == dct_recon
+
+    # in one-hot encoding scheme, if all entries are "hot" (i.e. nonzero),
+    # we should take the argmax
+    array[0] = 0.8  # tanh
+    array[1] = 0.6  # relu
+
+    config_recon = DenseConfiguration.from_array(cs_dense, array)
+    dct_recon = config_recon.get_dictionary()
+
+    assert dct_recon["activation_fn_1"] == "tanh"
